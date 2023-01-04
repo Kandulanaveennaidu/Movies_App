d713c8c35c67d17a5d28122fea2710613fb6e15b
Kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-01-04 09:20:28 +0530

    first commit

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,60 +5,60 @@ import Col from 'react-bootstrap/Col';
 import './style.css';
 
 import { BsFacebook, BsLinkedin, BsTwitter, BsStackOverflow, BsGoogle, BsGithub, BsYoutube } from "react-icons/bs";
-const FooterComponent = ()=>{
-    
+const FooterComponent = () => {
+
     return (
         <>
             <footer className='myFooter'>
                 <Container>
-                   <Row>
+                    <Row>
                         <Col className=''>
                             <h3 className='head_3'>Follow us :- </h3>
                             <ul className='socialIconsList'>
                                 <li>
-                                    <a rel="noreferrer" href="https://www.facebook.com/rohitazadmalik/" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsFacebook />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://www.linkedin.com/in/rohitazad/" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsLinkedin />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://twitter.com/rohitazad" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsTwitter />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://stackoverflow.com/users/1365428/rohit-azad-malik" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsStackOverflow />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://rohitazadmalik.blogspot.com/" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsGoogle />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://github.com/rohitazad" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsGithub />
                                     </a>
                                 </li>
                                 <li>
-                                    <a rel="noreferrer" href="https://www.youtube.com/c/AzadMalikRohit" target="_blank">
+                                    <a rel="noreferrer" href="" target="_blank">
                                         <BsYoutube />
                                     </a>
-                                </li> 
+                                </li>
                             </ul>
-                            
+
                         </Col>
                         <Col className='col-12'>
                             <div className="footerCopyright">
-                                <small>© Learn Coding With Bhai , 2022. Developed and Designed  by <a href="https://www.youtube.com/c/AzadMalikRohit" target="_blank" rel="noreferrer">Rohit Azad Malik (R.A.M)</a>.</small>
+                                <small>© Learn Coding With Bhai , 2022. Developed and Designed  by <a href="" target="_blank" rel="noreferrer">Rohit Azad Malik (R.A.M)</a>.</small>
                             </div>
                         </Col>
-                   </Row>
+                    </Row>
                 </Container>
             </footer>
         </>
